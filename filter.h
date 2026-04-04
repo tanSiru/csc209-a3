2f01751fd6519b7a265517b60f327d9911738967
SIRU TAN <siru0915@gmail.com>
2026-04-04 18:05:14 -0400

    implemented user input + renamed filter files to grayscale

--- filter.h
+++
@@ -1,9 +0,0 @@
-#ifndef GRAYSCALE_H
-#define GRAYSCALE_H
-
-#include "read_bmp.h"
-#include "encoding.h"
-
-struct pixel **apply_grayscale(struct pixel **pixel_array, int height, int width);
-
-#endif
--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "read_bmp.h"
-#include "filter.h"
+#include "grayscale.h"
 #include <string.h>
 
 int get_filter_choice(){
@@ -41,10 +41,14 @@ char *get_file_name(){
 
 
 int main(){
-    // int filter_choice = get_filter_choice();
-    // char *file_name = get_file_name();
+    int filter_choice = get_filter_choice();
+    char *file_name = get_file_name();
 
-    FILE *image = fopen("toronto.bmp", "rb");
+    FILE *image = fopen(file_name, "rb");
+    if(image==NULL){
+        printf("Invalid bitmap file name.  Please retry running the program with a proper path.");
+        exit(1);
+    }
     
     // Read in bitmap file metadata
     int pixel_array_offset, width, height;
@@ -54,30 +58,68 @@ int main(){
     struct pixel **pixels = read_bmp(image, pixel_array_offset, width, height);
     struct pixel **gray_pixels = apply_grayscale(pixels, height, width);
 
-    // write to bitmap
-    // use a different name than original so that a new bitmap will be created by the system
-    FILE *new_image= fopen("new_image.bmp", "wb");
+    if(filter_choice == 1){
+        // write to bitmap
+        // use a different name than original so that a new bitmap will be created by the system
+        FILE *new_image= fopen("grayscale.bmp", "wb");
 
-    // reset file pointer
-    rewind(image);
+        // reset file pointer
+        rewind(image);
 
-    // read in metadata of original bitmap then write it into the new bitmap
-    unsigned char header[pixel_array_offset];
-    fread(header, 1, pixel_array_offset, image);
-    fwrite(header, 1, pixel_array_offset, new_image);
+        // read in metadata of original bitmap then write it into the new bitmap
+        unsigned char header[pixel_array_offset];
+        fread(header, 1, pixel_array_offset, image);
+        fwrite(header, 1, pixel_array_offset, new_image);
 
-    // calculate the padding needed to make sure to make sure that the all bitmap row have size that is a multiple of 4
-    int padding = (4 - (width * 3) % 4) % 4;
-    unsigned char pad[3] = {0,0,0};
+        // calculate the padding needed to make sure to make sure that the all bitmap row have size that is a multiple of 4
+        int padding = (4 - (width * 3) % 4) % 4;
+        unsigned char pad[3] = {0,0,0};
 
-    for(int i = 0; i < height; i++){
-        // first write the pixels then add padding;
-        fwrite(gray_pixels[i], sizeof(struct pixel), width, new_image);
-        fwrite(pad, 1, padding, new_image);
+        for(int i = 0; i < height; i++){
+            // first write the pixels then add padding;
+            fwrite(gray_pixels[i], sizeof(struct pixel), width, new_image);
+            fwrite(pad, 1, padding, new_image);
+        }
+
+
+        // free(file_name);
+        fclose(image);
+        fclose(new_image);
+    }
+    else if(filter_choice == 2){
+        // ADD HORIZONTAL FLIP FILTER CODE
+    }
+    else if(filter_choice == 3){
+        FILE *new_image= fopen("grayscale.bmp", "wb");
+
+        // reset file pointer
+        rewind(image);
+
+        // read in metadata of original bitmap then write it into the new bitmap
+        unsigned char header[pixel_array_offset];
+        fread(header, 1, pixel_array_offset, image);
+        fwrite(header, 1, pixel_array_offset, new_image);
+
+        // calculate the padding needed to make sure to make sure that the all bitmap row have size that is a multiple of 4
+        int padding = (4 - (width * 3) % 4) % 4;
+        unsigned char pad[3] = {0,0,0};
+
+        for(int i = 0; i < height; i++){
+            // first write the pixels then add padding;
+            fwrite(gray_pixels[i], sizeof(struct pixel), width, new_image);
+            fwrite(pad, 1, padding, new_image);
+        }
+
+
+        // free(file_name);
+        fclose(image);
+        fclose(new_image);
+
+        printf("horizontal flip");
+
+        // ADD HORIZONTAL FLIP FILTER CODE
+
     }
 
-
-    // free(file_name);
-    fclose(image);
-    fclose(new_image);
+    
 }
