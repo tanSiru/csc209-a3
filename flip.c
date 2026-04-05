0eaa9de179d0bc686880b41f5f7bf1546c276893
Elizabeth <lisabethcl3@gmail.com>
2026-04-05 14:17:58 -0400

    cleaned up error-checking

--- flip.c
+++ flip.c
@@ -48,6 +48,101 @@ struct pixel *flip_row(struct pixel *row, int width) {
 }
 
 /*
+When a child crashes or write to child fails, spawn a new child and reassign the row given by row_index.
+*/
+void respond_error(int row_index, int width, struct pixel **pixels, struct pixel **new_pixels) {
+    // set up pipe for replacement child
+    int fd[2][2];
+
+    int child_pid;
+
+    if (pipe(fd[0]) == -1) {
+        perror("pipe");
+        exit(1);
+    }
+
+    if (pipe(fd[1]) == -1) {
+        perror("pipe");
+        exit(1);
+    }
+    
+    int new_worker = fork();
+
+    if (new_worker > 0) {
+        // store new child pid
+        child_pid = new_worker;
+
+        // close appropriate pipe ends
+        check_close(close(fd[0][0]));
+        check_close(close(fd[1][1]));
+        
+        // set up Message with failed row data for replacement child
+        Message *replace_row = malloc(sizeof(Message) + width * sizeof(struct pixel));
+        check_malloc(replace_row);
+        replace_row->row = row_index;
+        memcpy(replace_row->pixel_row, pixels[row_index], width * sizeof(struct pixel));
+
+        // write Message to replacement child
+        if (write(fd[0][1], replace_row, sizeof(Message) + width * sizeof(struct pixel))== -1) {
+            // if write to replacement worker fails, print error and end program
+            fprintf(stderr, "reassign row %d to child failed", row_index);  
+            exit(1);
+        } 
+
+        // close write pipe
+        check_close(close(fd[0][1]));
+
+        // read processed row from child
+        check_read(read(fd[1][0], replace_row, sizeof(Message) + width * sizeof(struct pixel)));
+
+        // place new row into new pixel array
+        new_pixels[row_index] = malloc(width * sizeof(struct pixel));
+        check_malloc(new_pixels[row_index]);
+        memcpy(new_pixels[row_index], replace_row->pixel_row, width * sizeof(struct pixel));
+        
+        int new_status;
+        if (waitpid(child_pid, &new_status, 0) == -1) {
+            perror("wait");
+            exit(1);
+        }
+
+        check_close(close(fd[1][0]));
+        free(replace_row);
+    }
+    else if (new_worker == 0) {
+        // close appropriate pipe ends
+        check_close(close(fd[0][1]));
+        check_close(close(fd[1][0]));
+
+        Message *new_row = malloc(sizeof(Message) + width * sizeof(struct pixel));
+
+        // read row data from parent
+        check_read(read(fd[0][0], new_row, sizeof(Message) + width * sizeof(struct pixel)));
+
+        // apply flip
+        struct pixel *flipped_row = flip_row(new_row->pixel_row, width);
+        memcpy(new_row->pixel_row, flipped_row, width * sizeof(struct pixel));
+
+        // close read pipe
+        check_close(close(fd[0][0]));
+
+        // write to parent
+        if (write(fd[1][1], new_row, sizeof(Message) + width * sizeof(struct pixel)) == -1) {
+            fprintf(stderr, "child write on row %d failed again", row_index);
+            exit(1);
+        }
+
+        // free Message, close write pipe
+        free(new_row);
+        check_close(close(fd[1][1]));
+    }
+    else { // fork failed
+        perror("fork");
+        exit(1);
+    }
+}
+
+/*
 Given an input pixel array, returns a pixel array where all the pixels have been flipped horizontally.
 */
 struct pixel **apply_flip(struct pixel **pixels, int height, int width) {
@@ -94,7 +189,13 @@ struct pixel **apply_flip(struct pixel **pixels, int height, int width) {
             memcpy(init_row->pixel_row, pixels[i], width * sizeof(struct pixel));   
 
             // write row of pixels to child for processing
-            check_write(write(fd[i][0][1], init_row, sizeof(Message) + width * sizeof(struct pixel)));
+            if (write(fd[i][0][1], init_row, sizeof(Message) + width * sizeof(struct pixel)) == -1) {
+                // if write fails, log the error
+                fprintf(stderr, "write row %d to child failed. reassigning...", i);
+
+                // spawn a new worker and reassign the row
+                respond_error(i, width, pixels, new_pixels);
+            }
             
             // close write pipe after writing the row
             check_close(close(fd[i][0][1]));
@@ -120,93 +221,13 @@ struct pixel **apply_flip(struct pixel **pixels, int height, int width) {
 
                     check_close(close(fd[i-(j-1)][1][0]));
                 }
-                else {  // child crashed -> make new child and redo row
-                    
-                    // set up pipes again for replacement child
-                    if (pipe(fd[i-j+1][0]) == -1) {
-                        perror("pipe");
-                        exit(1);
-                    }
+                else {  // child crashed
+                    // log error
+                    fprintf(stderr, "child for row %d crashed. reassigning row...", i-j+1);
 
-                    if (pipe(fd[i-j+1][1]) == -1) {
-                        perror("pipe");
-                        exit(1);
-                    }
-                    
-                    int new_worker = fork();
-
-                    if (new_worker > 0) {
-                        // store new child pid
-                        child_pids[i-j+1] = new_worker;
-
-                        // close appropriate pipe ends
-                        check_close(close(fd[i-j+1][0][0]));
-                        check_close(close(fd[i-j+1][1][1]));
-                        
-                        // set up Message with failed row data for replacement child
-                        Message *replace_row = malloc(sizeof(Message) + width * sizeof(struct pixel));
-                        check_malloc(replace_row);
-                        replace_row->row = i-j+1;
-                        memcpy(replace_row->pixel_row, pixels[i-j+1], width * sizeof(struct pixel));
-
-                        // write Message to replacement child
-                        if (write(fd[i-j+1][0][1], replace_row, sizeof(Message) + width * sizeof(struct pixel))== -1) {
-                            // if write to replacement worker fails, print error and end program
-                            fprintf(stderr, "reassign row to child failed");  
-                            exit(1);
-                        } 
-
-                        // close write pipe
-                        check_close(close(fd[i-j+1][0][1]));
-
-                        // read processed row from child
-                        check_read(read(fd[i-j+1][1][0], replace_row, sizeof(Message) + width * sizeof(struct pixel)));
-
-                        // place new row into new pixel array
-                        new_pixels[i-j+1] = malloc(width * sizeof(struct pixel));
-                        memcpy(new_pixels[i-j+1], replace_row->pixel_row, width * sizeof(struct pixel));
-                        
-                        int new_status;
-                        if (waitpid(child_pids[i-j+1], &new_status, 0) == -1) {
-                            perror("wait");
-                            exit(1);
-                        }
-
-                        check_close(close(fd[i-j+1][1][0]));
-                        free(replace_row);
-                    }
-                    else if (new_worker == 0) {
-                        // close appropriate pipe ends
-                        check_close(close(fd[i-j+1][0][1]));
-                        check_close(close(fd[i-j+1][1][0]));
-
-                        Message *new_row = malloc(sizeof(Message) + width * sizeof(struct pixel));
-
-                        // read row data from parent
-                        check_read(read(fd[i-j+1][0][0], new_row, sizeof(Message) + width * sizeof(struct pixel)));
-
-                        // apply flip
-                        struct pixel *flipped_row = flip_row(new_row->pixel_row, width);
-                        memcpy(new_row->pixel_row, flipped_row, width * sizeof(struct pixel));
-
-                        // close read pipe
-                        check_close(close(fd[i-j+1][0][0]));
-
-                        // write to parent
-                        if (write(fd[i-j+1][1][1], new_row, sizeof(Message) + width * sizeof(struct pixel)) == -1) {
-                            fprintf(stderr, "child write failed again");
-                            exit(1);
-                        }
-
-                        // free Message, close write pipe
-                        free(new_row);
-                        check_close(close(fd[i-j+1][1][1]));
-                    }
-                    else { // fork failed
-                        perror("fork");
-                        exit(1);
-                    }
-                 }
+                    // respawn and reassign
+                    respond_error(i-j+1, width, pixels, new_pixels);
+                }
                 }
                 worker_count = 0; // parent resets count of active workers after collecting all of them
             }
@@ -274,90 +295,12 @@ struct pixel **apply_flip(struct pixel **pixels, int height, int width) {
 
                 check_close(close(fd[height-j][1][0]));
             }
-            else {  // child crashed -> make new child and redo row
-                // set up pipes again for replacement child
-                if (pipe(fd[height-j][0]) == -1) {
-                    perror("pipe");
-                    exit(1);
-                }
-                if (pipe(fd[height-j][1]) == -1) {
-                    perror("pipe");
-                    exit(1);
-                }
-                
-                int new_worker = fork();
+            else {  // child crashed
+                // log error
+                fprintf(stderr, "child for row %d crashed. reassigning row...", height-j);
 
-                if (new_worker > 0) {
-                    // store new child pid
-                    child_pids[height-j] = new_worker;
-
-                    // close appropriate pipe ends
-                    check_close(close(fd[height-j][0][0]));
-                    check_close(close(fd[height-j][1][1]));
-                    
-                    // set up Message with failed row data for replacement child
-                    Message *replace_row = malloc(sizeof(Message) + width * sizeof(struct pixel));
-                    check_malloc(replace_row);
-                    replace_row->row = height-j;
-                    memcpy(replace_row->pixel_row, pixels[height-j], width * sizeof(struct pixel));
-
-                    // write Message to replacement child
-                    if (write(fd[height-j][0][1], replace_row, sizeof(Message) + width * sizeof(struct pixel))== -1) {
-                            // if write to replacement worker fails, print error and end program
-                            fprintf(stderr, "reassign row to child failed");  
-                            exit(1);
-                        } 
-
-                    // close write pipe
-                    check_close(close(fd[height-j][0][1]));
-
-                    // read processed row from child
-                    check_read(read(fd[height-j][1][0], replace_row, sizeof(Message) + width * sizeof(struct pixel)));
-
-                    // place new row into new pixel array
-                    new_pixels[height-j] = malloc(width * sizeof(struct pixel));
-                    memcpy(new_pixels[height-j], replace_row->pixel_row, width * sizeof(struct pixel));
-                    
-                    int new_status;
-                    if (waitpid(child_pids[height-j], &new_status, 0) == -1) {
-                        perror("wait");
-                        exit(1);
-                    }
-
-                    check_close(close(fd[height-j][1][0]));
-                    free(replace_row);
-                }
-                else if (new_worker == 0) {
-                    // close appropriate pipe ends
-                    check_close(close(fd[height-j][0][1]));
-                    check_close(close(fd[height-j][1][0]));
-
-                    Message *new_row = malloc(sizeof(Message) + width * sizeof(struct pixel));
-
-                    // read row data from parent
-                    check_read(read(fd[height-j][0][0], new_row, sizeof(Message) + width * sizeof(struct pixel)));
-
-                    // apply flip
-                    struct pixel *flipped_row = flip_row(new_row->pixel_row, width);
-                    memcpy(new_row->pixel_row, flipped_row, width * sizeof(struct pixel));
-
-                    // close read pipe
-                    check_close(close(fd[height-j][0][0]));
-
-                    // write to parent
-                    if (write(fd[height-j][1][1], new_row, sizeof(Message) + width * sizeof(struct pixel)) == -1) {
-                        fprintf(stderr, "child write failed again");
-                        exit(1);
-                    }
-
-                    // free Message, close write pipe
-                    free(new_row);
-                    check_close(close(fd[height-j][1][1]));
-                }
-                else { // fork failed
-                    perror("fork");
-                    exit(1);
-                }
+                // respawn and reassign
+                respond_error(height-j, width, pixels, new_pixels);
             }
         }
 
