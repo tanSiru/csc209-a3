8ab573c9f35b7d96dab4ed61438d7197f994dcc4
SIRU TAN <siru0915@gmail.com>
2026-04-04 20:55:17 -0400

    trying fix

--- grayscale.c
+++ grayscale.c
@@ -181,192 +181,203 @@ struct pixel **apply_grayscale(struct pixel **pixel_array, int height, int width
         pipe_fd[i][][1] -> write fd
         */ 
         
-        int pipe_fd[height][2][2];
-        pid_t child_pids[height]; // keep an array of child pids so later we can use waitpid to synchronize wait calls
-
-        for(int i = 0; i <height; i++) {
-                if(pipe(pipe_fd[i][0])==-1){
-                        // ASK HOW WE WILL HANDLE ERROR
-                        perror("pipe");
-                        exit(1);
-                }
-
-                if(pipe(pipe_fd[i][1])==-1){
-                        perror("pipe");
-                        exit(1);
-                }
-
-                pid_t result = fork();
-
-
-                struct pixel *cur_row = pixel_array[i];
-
-                if(result>0){
-                        printf("parent created child for row %d\n", i);
-                        fflush(stdout);
-
-                        close(pipe_fd[i][1][1]); // close the pipe child->parent for writing
-                        close(pipe_fd[i][0][0]); // close the pipe parent->child for reading
-
-                        child_pids[i] = result; // store the pid of the child responsible for row i, allowing us to wait for the correct child for pipe_fd[i]
-
-                        // call helper function to make the message that will be sent to pipe
-                        Message *row_to_send = make_message(cur_row, i, width);
-                        if(row_to_send == NULL){
-                                perror("malloc");
-                                exit(1);
-                        }
-
-                        // write the encoded data into pipe
-                        if(write_message(pipe_fd[i][0][1], row_to_send, width) == -1){
-                                perror("write");
-                                exit(1);
-                        }
-                        
-                        free(row_to_send);
-                        close(pipe_fd[i][0][1]); // close write pipe after everything has been written
-                }
-
-
-                if(result < 0) {
-                        printf("Error");
-                        exit(1);
-                } else if (result == 0) {
-                        printf("child processing row %d\n", i);
-                        fflush(stdout);
-                        close(pipe_fd[i][1][0]); // close the pipe child->parent for reading
-                        close(pipe_fd[i][0][1]); // close the pipe parent->child for writing
-
-                        
-
-                        // call helper function to handle processing the row and sending the processed row to pipe
-                        int child_status = child_process_row(pipe_fd[i][0][0], pipe_fd[i][1][1], width);
-                
-                        // close all the pipes as the child has no more work to do
-                        close(pipe_fd[i][1][1]); 
-                        close(pipe_fd[i][0][0]); 
-                        printf("child done row %d\n", i);
-                        exit(child_status);
-                }
-        }
+        int max_workers = 3; // maximum # of children that can be created at any given to not blow up the file descriptors through pipe
 
         // stores the reconstructed pixel array after every row had a gray filter applied to it
         struct pixel **modified_pixel_array = malloc(height*sizeof(struct pixel *));
+        if(modified_pixel_array == NULL){
+                perror("malloc");
+                exit(1);
+        }
 
-        //
-        for(int i = 0; i < height; i++) {
-                int status;
-                // use the child_pids array from earlier to synchronize the child process with the corresponding pipe
-                printf("parent waiting for row %d\n", i);
-                pid_t childpid = waitpid(child_pids[i], &status, 0);
-                printf("parent finished waiting for row %d\n", i);
-                if(childpid == -1){
-                        perror("wait");
+        for(int start = 0; start < height; start += max_workers){
+
+                int max_rows = max_workers;
+                if(start + max_rows > height){
+                        max_rows = height - start;
                 }
-                if(WIFEXITED(status) && WEXITSTATUS(status)==0){
-                        // read in the processed row by calling helper function
-                        printf("parent reading row %d\n", i);
-                        Message *received_msg = read_message(pipe_fd[i][1][0], width);
-                        if(received_msg == NULL){
-                                perror("read");
-                                exit(1);
-                        }
 
-                        // write the processed row into modified_pixel_array using helper function
-                        copy_message_into_array(modified_pixel_array, received_msg, width);
+                int pipe_fd[max_rows][2][2];
+                pid_t child_pids[max_rows]; // keep an array of child pids so later we can use waitpid to synchronize wait calls
 
-                        free(received_msg);
+                for(int k = 0; k < max_rows; k++) {
+                        int i = start + k;
 
-                        close(pipe_fd[i][1][0]);
-                }else{
-                        /*
-                        pipe_fd[0][0] parent -> child
-                        pipe_fd[1][1] child -> parent
-
-
-                        pipe_fd[i][][0] -> read fd
-                        pipe_fd[i][][1] -> write fd
-                        */ 
-
-                        /*
-                        failure due to exit or signalling will both be handled in the same way
-                        handle failure by creating a new child to work on that row
-                        the logic is pretty much the exact same so the code is basically copied over
-                        */
-                        if(pipe(pipe_fd[i][0])==-1){
+                        if(pipe(pipe_fd[k][0])==-1){
+                                // ASK HOW WE WILL HANDLE ERROR
                                 perror("pipe");
                                 exit(1);
                         }
 
-                        if(pipe(pipe_fd[i][1])==-1){
+                        if(pipe(pipe_fd[k][1])==-1){
                                 perror("pipe");
                                 exit(1);
                         }
 
-                        pid_t new_child = fork();
+                        pid_t result = fork();
+
 
                         struct pixel *cur_row = pixel_array[i];
 
-                        if(new_child > 0){
-                                close(pipe_fd[i][1][1]); // close child->parent write
-                                close(pipe_fd[i][0][0]); // close parent->child read
+                        if(result>0){
+                                close(pipe_fd[k][1][1]); // close the pipe child->parent for writing
+                                close(pipe_fd[k][0][0]); // close the pipe parent->child for reading
 
-                                child_pids[i] = new_child;
+                                child_pids[k] = result; // store the pid of the child responsible for row i, allowing us to wait for the correct child for pipe_fd[i]
 
+                                // call helper function to make the message that will be sent to pipe
                                 Message *row_to_send = make_message(cur_row, i, width);
                                 if(row_to_send == NULL){
                                         perror("malloc");
                                         exit(1);
                                 }
 
-                                if(write_message(pipe_fd[i][0][1], row_to_send, width) == -1){
+                                // write the encoded data into pipe
+                                if(write_message(pipe_fd[k][0][1], row_to_send, width) == -1){
                                         perror("write");
                                         exit(1);
                                 }
-
+                                
                                 free(row_to_send);
-                                close(pipe_fd[i][0][1]);
-
-                                // wait again for replacement child
-                                int new_status;
-                                pid_t childpid2 = waitpid(new_child, &new_status, 0);
-                                if(childpid2 == -1){
-                                        perror("wait");
-                                        exit(1);
-                                }
-
-                                if(WIFEXITED(new_status) && WEXITSTATUS(new_status) == 0){
-                                        Message *received_msg = read_message(pipe_fd[i][1][0], width);
-                                        if(received_msg == NULL){
-                                                perror("read");
-                                                exit(1);
-                                        }
-
-                                        copy_message_into_array(modified_pixel_array, received_msg, width);
-
-                                        free(received_msg);
-                                        close(pipe_fd[i][1][0]);
-                                }else{
-                                        fprintf(stderr, "Row %d failed again\n", i);
-                                        exit(1);
-                                }
+                                close(pipe_fd[k][0][1]); // close write pipe after everything has been written
                         }
 
-                        if(new_child < 0){
-                                perror("fork");
+
+                        if(result < 0) {
+                                printf("Error");
                                 exit(1);
-                        } else if(new_child == 0){
-                                close(pipe_fd[i][1][0]);
-                                close(pipe_fd[i][0][1]);
+                        } else if (result == 0) {
+                                close(pipe_fd[k][1][0]); // close the pipe child->parent for reading
+                                close(pipe_fd[k][0][1]); // close the pipe parent->child for writing
 
-                                int child_status = child_process_row(pipe_fd[i][0][0], pipe_fd[i][1][1], width);
+                                
 
-                                close(pipe_fd[i][1][1]);
-                                close(pipe_fd[i][0][0]);
+                                // call helper function to handle processing the row and sending the processed row to pipe
+                                int child_status = child_process_row(pipe_fd[k][0][0], pipe_fd[k][1][1], width);
+                        
+                                // close all the pipes as the child has no more work to do
+                                close(pipe_fd[k][1][1]); 
+                                close(pipe_fd[k][0][0]); 
                                 exit(child_status);
                         }
                 }
+
+                //
+                for(int k = 0; k < max_rows; k++) {
+                        int i = start + k;
+                        int status;
+                        // use the child_pids array from earlier to synchronize the child process with the corresponding pipe
+                        pid_t childpid = waitpid(child_pids[k], &status, 0);
+                        if(childpid == -1){
+                                perror("wait");
+                        }
+
+                        if(WIFEXITED(status) && WEXITSTATUS(status)==0){
+                                // read in the processed row by calling helper function
+                                Message *received_msg = read_message(pipe_fd[k][1][0], width);
+                                if(received_msg == NULL){
+                                        perror("read");
+                                        exit(1);
+                                }
+
+                                // write the processed row into modified_pixel_array using helper function
+                                copy_message_into_array(modified_pixel_array, received_msg, width);
+
+                                free(received_msg);
+
+                                close(pipe_fd[k][1][0]);
+                        }else{
+                                /*
+                                pipe_fd[0][0] parent -> child
+                                pipe_fd[1][1] child -> parent
+
+
+                                pipe_fd[i][][0] -> read fd
+                                pipe_fd[i][][1] -> write fd
+                                */ 
+
+                                /*
+                                failure due to exit or signalling will both be handled in the same way
+                                handle failure by creating a new child to work on that row
+                                the logic is pretty much the exact same so the code is basically copied over
+                                */
+                                if(pipe(pipe_fd[k][0])==-1){
+                                        perror("pipe");
+                                        exit(1);
+                                }
+
+                                if(pipe(pipe_fd[k][1])==-1){
+                                        perror("pipe");
+                                        exit(1);
+                                }
+
+                                pid_t new_child = fork();
+
+                                struct pixel *cur_row = pixel_array[i];
+
+                                if(new_child > 0){
+                                        close(pipe_fd[k][1][1]); // close child->parent write
+                                        close(pipe_fd[k][0][0]); // close parent->child read
+
+                                        child_pids[k] = new_child;
+
+                                        Message *row_to_send = make_message(cur_row, i, width);
+                                        if(row_to_send == NULL){
+                                                perror("malloc");
+                                                exit(1);
+                                        }
+
+                                        if(write_message(pipe_fd[k][0][1], row_to_send, width) == -1){
+                                                perror("write");
+                                                exit(1);
+                                        }
+
+                                        free(row_to_send);
+                                        close(pipe_fd[k][0][1]);
+
+                                        // wait again for replacement child
+                                        int new_status;
+                                        pid_t childpid2 = waitpid(new_child, &new_status, 0);
+                                        if(childpid2 == -1){
+                                                perror("wait");
+                                                exit(1);
+                                        }
+
+                                        if(WIFEXITED(new_status) && WEXITSTATUS(new_status) == 0){
+                                                Message *received_msg = read_message(pipe_fd[k][1][0], width);
+                                                if(received_msg == NULL){
+                                                        perror("read");
+                                                        exit(1);
+                                                }
+
+                                                copy_message_into_array(modified_pixel_array, received_msg, width);
+
+                                                free(received_msg);
+                                                close(pipe_fd[k][1][0]);
+                                        }else{
+                                                fprintf(stderr, "Row %d failed again\n", i);
+                                                exit(1);
+                                        }
+                                }
+
+                                if(new_child < 0){
+                                        perror("fork");
+                                        exit(1);
+                                } else if(new_child == 0){
+                                        close(pipe_fd[k][1][0]);
+                                        close(pipe_fd[k][0][1]);
+
+                                        int child_status = child_process_row(pipe_fd[k][0][0], pipe_fd[k][1][1], width);
+
+                                        close(pipe_fd[k][1][1]);
+                                        close(pipe_fd[k][0][0]);
+                                        exit(child_status);
+                                }
+                        }
+                }
         }
+
         return modified_pixel_array;
 }
+        
 
